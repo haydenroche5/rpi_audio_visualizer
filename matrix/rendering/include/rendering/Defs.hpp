d8236b975bc60312bb72b4cd05270e05cdc3a7ee
Hayden Roche <haydenroche5@gmail.com>
2019-11-01 13:27:21 +0000

    fix to rendering/Defs.hpp from pi

--- matrix/rendering/include/rendering/Defs.hpp
+++ matrix/rendering/include/rendering/Defs.hpp
@@ -3,6 +3,7 @@
 #include "Color.hpp"
 #include "led_matrix/led-matrix.h"
 #include <array>
+#include <boost/lockfree/spsc_queue.hpp>
 
 namespace matrix
 {
@@ -13,7 +14,9 @@ static constexpr size_t NUM_COLS{64};
 
 template <size_t NUM_BARS> using FreqBarUpdateT = std::array<int, NUM_BARS>;
 static constexpr size_t FREQ_BAR_UPDATE_QUEUE_DEPTH{4};
-using FreqBarUpdateQueueT = boost::lockfree::spsc_queue<FreqBarUpdateT>;
+template <size_t NUM_BARS>
+using FreqBarUpdateQueueT =
+    boost::lockfree::spsc_queue<FreqBarUpdateT<NUM_BARS>>;
 using FrameBufferT = rgb_matrix::FrameCanvas;
 // using AvgOctavePowersT = std::array<double, NUM_OCTAVES>;
 } // namespace rendering
