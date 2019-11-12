7fdbd5ebef5f98aaab8f34d70641d09ba8804412
Hayden Roche <haydenroche5@gmail.com>
2019-11-11 18:33:57 -0600

    more dependencies cleanup

--- dependencies/cmake/modules/FindFftw3.cmake
+++ dependencies/cmake/modules/FindFftw3.cmake
@@ -1,6 +1,3 @@
 pkg_check_modules(Fftw3 REQUIRED IMPORTED_TARGET GLOBAL fftw3)
 get_target_property(Fftw3_include_dirs PkgConfig::Fftw3 INTERFACE_INCLUDE_DIRECTORIES)
 get_target_property(Fftw3_link_libs PkgConfig::Fftw3 INTERFACE_LINK_LIBRARIES)
-message("Fftw3_include_dirs: ${Fftw3_include_dirs}")
-message("Fftw3_link_libs: ${Fftw3_link_libs}")
-message("Fftw3_LINK_LIBRARIES: ${Fftw3_LINK_LIBRARIES}")
