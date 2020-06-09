253027e272d5404ee04d7f97ab8278193d1b735c
Hayden Roche <haydenroche5@gmail.com>
2020-06-09 18:45:55 -0500

    latest visualizer executable

--- bin/src/visualizer.cpp
+++ bin/src/visualizer.cpp
@@ -23,42 +23,74 @@ float MaxDbSeen{0};
 float MinDbSeen{1000};
 
 constexpr float MIN_MAGNITUDE{0};
-constexpr float MAX_MAGNITUDE{100};
+constexpr float MAX_MAGNITUDE{5};
+static constexpr size_t NUM_BARS{32};
+static constexpr size_t NUM_COLORS_PER_GRADIENT{16};
+static constexpr size_t ANIMATION_SPEED{8};
+static constexpr float PRE_EMPHASIS_FACTOR{0.97};
 
-static constexpr size_t NUM_BARS{16};
-static constexpr size_t NUM_COLORS_PER_GRADIENT{8};
-static constexpr size_t ANIMATION_SPEED{2};
 static constexpr std::array<float, NUM_BARS> OCTAVE_BOUNDARIES{
-    18.581,  26.278,  37.163,  52.556,  74.325,   105.112,  148.651,  210.224,
-    297.302, 420.448, 594.604, 840.896, 1189.207, 1681.793, 2378.414, 3363.586};
+    14.1, 17.8, 22.4, 28.2, 35.5, 44.7, 56.2, 70.8, 89.1, 112,  141,
+    178,  224,  282,  355,  447,  562,  708,  891,  1122, 1413, 1778,
+    2239, 2818, 3548, 4467, 5623, 7079, 9999, 9999, 9999, 9999};
 
 std::atomic<bool> Terminate{false};
 
-// TODO: Overlap windows by 50%
 template <size_t NUM_BARS>
 void updatePositions(AudioQueueT &aAudioQueue,
                      VisualizerUpdateQueueT<NUM_BARS> &aBarUpdateQueue)
 {
+    BufferT myPreEmphasizedBuffer{};
     FftInputArrayT myFftInput{SAMPLES_PER_BUFFER, FFT_ALIGNMENT};
     FftOutputArrayT myFftOutput{FFT_POINTS_REAL, FFT_ALIGNMENT};
     std::array<float, NUM_BARS> myMaxMagnitudePerOctave{};
+    VisualizerBarPositionsT<NUM_BARS> myNewPositions{};
+    // std::array<SampleT, SAMPLES_PER_BUFFER / 2> myPreviousBuffer{};
+    // bool myPreviousBufferExists{false};
 
     while (!Terminate)
     {
         if (aAudioQueue.read_available() > 0)
         {
-            const auto &myBuffer{aAudioQueue.front()};
+            const auto &myCurrentBuffer{aAudioQueue.front()};
+
+            myPreEmphasizedBuffer[0] = myCurrentBuffer[0];
+
+            for (size_t i{1}; i < SAMPLES_PER_BUFFER; ++i)
+            {
+                myPreEmphasizedBuffer[i] =
+                    myCurrentBuffer[i] -
+                    PRE_EMPHASIS_FACTOR * myCurrentBuffer[i - 1];
+            }
+
+            // Done with the original buffer, can pop.
+            aAudioQueue.pop();
 
             // Window samples with a Hann window.
             for (size_t i{0}; i < SAMPLES_PER_BUFFER; ++i)
             {
                 myFftInput[i] =
-                    myBuffer[i] * 0.5 *
+                    myPreEmphasizedBuffer[i] * 0.5 *
                     (1 - std::cos((2 * M_PI * i) / SAMPLES_PER_BUFFER));
             }
 
-            // Done with the original buffer, can pop.
-            aAudioQueue.pop();
+            // // Overlap with the latter half of the previous buffer
+            // if (myPreviousBufferExists)
+            // {
+            //     std::copy(myPreviousBuffer.begin(), myPreviousBuffer.end(),
+            //               myOverlappedBuffer.begin());
+            //     std::copy(myCurrentBuffer.begin(),
+            //               myCurrentBuffer.begin() + SAMPLES_PER_BUFFER / 2,
+            //               myOverlappedBuffer.begin() + SAMPLES_PER_BUFFER /
+            //               2);
+            // }
+            // else
+            // {
+            //     myOverlappedBuffer = myCurrentBuffer;
+            //     myPreviousBufferExists = true;
+            // }
+            // std::copy(myCurrentBuffer.begin() + SAMPLES_PER_BUFFER / 2,
+            //           myCurrentBuffer.end(), myPreviousBuffer.begin());
 
             // Take the Fourier transform of the samples.
             fftwpp::rcfft1d myFft(SAMPLES_PER_BUFFER, myFftInput, myFftOutput);
@@ -72,6 +104,8 @@ void updatePositions(AudioQueueT &aAudioQueue,
             {
                 auto myFrequency{i * FFT_FUNDAMENTAL_FREQ};
 
+                // TODO: Try some measure of the power in each octave instead of
+                // max magnitude
                 if (myFrequency >= OCTAVE_BOUNDARIES[myOctaveBoundaryIdx])
                 {
                     if (myOctaveBoundaryIdx == NUM_BARS - 1)
@@ -96,7 +130,6 @@ void updatePositions(AudioQueueT &aAudioQueue,
             // Compute the position of each bar. Each bar corresponds to one
             // octave. The height of the bar is directly proportional to the
             // maximum FFT magnitude in the octave.
-            VisualizerBarPositionsT<NUM_BARS> myNewPositions{};
             for (size_t i{0}; i < NUM_BARS; ++i)
             {
                 auto myScaledMagnitude{
@@ -188,8 +221,9 @@ int main(int argc, char *argv[])
         FREQ_BAR_UPDATE_QUEUE_DEPTH};
     using VisualizerT =
         Visualizer<NUM_BARS, NUM_COLORS_PER_GRADIENT, ANIMATION_SPEED>;
+    constexpr bool ENABLE_PROFILING{false};
     VisualizerT myVisualizer{myMatrixOptions, myRuntimeOptions,
-                             myBarUpdateQueue, false};
+                             myBarUpdateQueue, ENABLE_PROFILING};
 
     boost::thread myVisualizerThread(animate<VisualizerT>,
                                      std::ref(myVisualizer));
